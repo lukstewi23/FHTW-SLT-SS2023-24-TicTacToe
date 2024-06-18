4169e0040bdaef000f3d7d58d824de68315187ab
Lukas Steurer <wi23b019@technikum-wien.at>
2024-06-18 20:28:27 +0200

    added a small greeting - SLT04  has been implemented on a push onto main already by accident, the main branch protection has been fixed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Welcome to Tic Tac Toe!");
         TicTacToe game = new TicTacToe();
         Scanner scanner = new Scanner(System.in);
 
