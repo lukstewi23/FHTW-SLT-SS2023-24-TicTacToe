72894591a9c58396f99ff3a65c93b6851d60208e
Lukas Steurer <wi23b019@technikum-wien.at>
2024-06-18 20:44:12 +0200

    fixing tests

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -4,8 +4,12 @@ public class Board {
     private char[][] cells;
 
     public Board() {
-        cells = new char[3][3];
-        clear();
+        this.cells = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cells[i][j] = '-';
+            }
+        }
     }
 
     public boolean isCellEmpty(int row, int col) {
