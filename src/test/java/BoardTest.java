13f5b0426bf828af7256cc6a6ca495866dc41be2
Lukas Steurer <wi23b019@technikum-wien.at>
2024-06-18 20:48:53 +0200

    fixing tests

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -12,30 +12,56 @@ public class BoardTest {
     }
 
     @Test
-    public void testIsCellEmpty() {
+    public void testIsCellEmpty_Positive() {
         assertTrue(board.isCellEmpty(0, 0));
+    }
+
+    @Test
+    public void testIsCellEmpty_Negative() {
         board.place(0, 0, 'X');
         assertFalse(board.isCellEmpty(0, 0));
     }
 
     @Test
-    public void testPlace() {
+    public void testPlace_Positive() {
         board.place(0, 0, 'X');
         assertFalse(board.isCellEmpty(0, 0));
     }
 
     @Test
-    public void testClear() {
+    public void testPlace_Negative() {
+        assertThrows(IllegalArgumentException.class, () -> board.place(3, 3, 'X'));
+    }
+
+    @Test
+    public void testClear_Positive() {
         board.place(0, 0, 'X');
         board.clear();
         assertTrue(board.isCellEmpty(0, 0));
     }
 
     @Test
-    public void testToString() {
+    public void testClear_Negative() {
+        board.clear();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                assertTrue(board.isCellEmpty(i, j));
+            }
+        }
+    }
+
+    @Test
+    public void testToString_Positive() {
         String boardString = board.toString();
         assertEquals(29, boardString.length());
         assertTrue(boardString.contains("|"));
         assertTrue(boardString.contains("-"));
+    }
+
+    @Test
+    public void testToString_Negative() {
+        String boardString = board.toString();
+        assertFalse(boardString.contains("X"));
+        assertFalse(boardString.contains("O"));
     }
 }
